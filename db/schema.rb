3f45f75d4606bd9f657dd232aa03a431298b4b4c
Jesse McKay <jessemckay@me.com>
2014-03-03 14:13:12 -0500

    t

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140227182458) do
+ActiveRecord::Schema.define(version: 20140303183505) do
 
   create_table "admin_users", force: true do |t|
     t.string   "first_name",      limit: 25
