9229365bc2ef8b7a819ac56b396a7c1d0db8cc1c
Jesse McKay <jessemckay@me.com>
2014-03-03 13:56:55 -0500

    Fixed typo in id

--- db/migrate/20140303183505_create_admin_users_pages_join.rb
+++ db/migrate/20140303183505_create_admin_users_pages_join.rb
@@ -1,6 +1,6 @@
 class CreateAdminUsersPagesJoin < ActiveRecord::Migration
   def change
-    create_table :admin_users_pages, id => false do |t|
+    create_table :admin_users_pages, :id => false do |t|
       t.integer "admin_user_id"
       t.integer "page_id"
     end
